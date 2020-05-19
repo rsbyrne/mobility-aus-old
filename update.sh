6449d275c38ad0859e79b2dc8eac1b916cb4f093
rsbyrne <rohan.byrne@gmail.com>
2020-05-19 13:47:37 +0000

    Automatic push.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MOUNTFROM=$PWD
+MOUNTFROM=$PWD'/..'
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/mobility-aus'
 SOCK='/var/run/docker.sock'
-docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK -it --shm-size 2g $IMAGE sudo python3 update.py
+docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK -it --shm-size 2g $IMAGE sudo mobility-aus/python3 update.py
