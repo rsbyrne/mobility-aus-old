142b4432046c2bd86613d252e376e8039717ed98
rsbyrne <rohan.byrne@gmail.com>
2020-05-19 14:08:49 +0000

    Automatic push.

--- update.py
+++ update.py
@@ -1,3 +1,12 @@
+import sys
+import os
+parentDir = os.path.dirname(__file__)
+if not parentDir in sys.path:
+    sys.path.append(parentDir)
+parentDir = os.path.dirname(parentDir)
+if not parentDir in sys.path:
+    sys.path.append(parentDir)
+
 import produce
 
 regions = {'vic', 'mel', 'syd', 'nsw', 'ade', 'sa', 'per', 'wa', 'tas', 'qld', 'nt'}
