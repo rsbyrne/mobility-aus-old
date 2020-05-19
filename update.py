e845fbfc88f432dabb2b4df6cc73a5d5dc2909f4
rsbyrne <rohan.byrne@gmail.com>
2020-05-19 14:15:05 +0000

    Automatic push.

--- update.py
+++ update.py
@@ -1,16 +1,8 @@
-import sys
-import os
-parentDir = os.path.dirname(__file__)
-if not parentDir in sys.path:
-    sys.path.append(parentDir)
-parentDir = os.path.dirname(parentDir)
-if not parentDir in sys.path:
-    sys.path.append(parentDir)
-
 import produce
 
-regions = {'vic', 'mel', 'syd', 'nsw', 'ade', 'sa', 'per', 'wa', 'tas', 'qld', 'nt'}
-refresh = True
+#regions = {'vic', 'mel', 'syd', 'nsw', 'ade', 'sa', 'per', 'wa', 'tas', 'qld', 'nt'}
+regions = {'vic', 'mel', 'nsw', 'syd'}
+refresh = False
 
 for region in regions:
     mob = produce.get_mob_lga_date(region, refresh = refresh)
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MOUNTFROM=$PWD'/..'
+MOUNTFROM=$PWD
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/mobility-aus'
 SOCK='/var/run/docker.sock'
-docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g $IMAGE sudo python3 '/home/morpheus/workspace/mount/mobility-aus/update.py'
+docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g $IMAGE sudo python3 '/home/morpheus/workspace/mount/update.py'
