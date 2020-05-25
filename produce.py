43ea5631e66bd04338a842983f6d249c1d594dd0
rsbyrne <rohan.byrne@gmail.com>
2020-05-25 01:03:26 +0000

    Automatic push.

--- produce.py
+++ produce.py
@@ -105,11 +105,10 @@ def make_mob_lga_dateMap(raw, region):
     frm['name'] = lgas.loc[frm.index]['LGA_NAME19']
     frm['area'] = lgas.loc[frm.index]['AREASQKM19']
 
-    if len(frm) > 50:
-        scale = np.sqrt(np.median(frm['geometry'].area))
-        scalingCoeff = 1e-2
-        frm['geometry'] = frm['geometry'].simplify(scale * scalingCoeff)
-        frm['geometry'] = frm['geometry'].buffer(scale * 0.5 * scalingCoeff)
+    scale = np.sqrt(np.median(frm['geometry'].area))
+    scalingCoeff = len(frm) ** 2. * 1e-5
+    frm['geometry'] = frm['geometry'].simplify(scale * scalingCoeff)
+    frm['geometry'] = frm['geometry'].buffer(scale * 0.5 * scalingCoeff)
 
     frm.columns = [
         str(round(int(n.to_numpy()) / 1e6)) if type(n) is pd.Timestamp else n
