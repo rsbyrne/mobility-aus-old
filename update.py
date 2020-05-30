e5534f92e03b08be2effa0d8d0d4e599e01dc0b7
root <root@miranda-1.novalocal>
2020-05-30 06:43:47 +0000

    Automatic push.

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ regions = {
 
 for region in regions:
     try:
-        mob = produce.get_mob_lga_date(region, get = False, refresh = True, override = True)
+        mob = produce.get_mob_lga_date(region, get = False, refresh = True, override = False)
         produce.make_mob_plots(mob, region)
         produce.make_mob_lga_dateMap(mob, region)
     except NoData:
