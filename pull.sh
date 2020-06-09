21f3a51b97159ea6573eab6f998d292515e5e34c
rsbyrne <rohan.byrne@gmail.com>
2020-06-09 23:43:59 +0000

    Automatic push.

--- pull.sh
+++ pull.sh
@@ -4,6 +4,6 @@ MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/mobility-aus'
 SOCK='/var/run/docker.sock'
 sudo chmod -R 777 'data'
-sudo rm geckodriver.log
+sudo rm -f geckodriver.log
 docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g $IMAGE python3 '/home/morpheus/workspace/mount/pull.py'
 sudo chmod -R 777 'data'
