4dc3de9fdf6ceba79a4bf024bac264ba47de0969
root <root@miranda-1.novalocal>
2020-08-19 06:29:25 +0000

    Automatic push.

--- update.py
+++ update.py
@@ -9,30 +9,30 @@ aggTypes = {
     'lga', 'sa2', 'postcodes'
     }
 
-# absLookup = produce.get_abs_lookup(aggTypes, refresh = True)
-# absLookup = produce.get_abs_lookup({'lga', 'sa2'})
-# for region in regions:
-#     for aggType in aggTypes:
-#         try:
-#             mob = produce.get_mob_date(
-#                 region,
-#                 aggType,
-#                 get = False,
-#                 refresh = True,
-#                 override = False
-#                 )
-#             produce.make_mob_plots(
-#                 mob,
-#                 region,
-#                 aggType,
-#                 )
-#             produce.make_mob_dateMap(
-#                 mob,
-#                 region,
-#                 aggType,
-#                 )
-#         except NoData:
-#             print("No data currently available for:", region)
-#         except:
-#             print("Something went wrong with:", region, aggType)
+absLookup = produce.get_abs_lookup(aggTypes, refresh = True)
+absLookup = produce.get_abs_lookup({'lga', 'sa2'})
+for region in regions:
+    for aggType in aggTypes:
+        try:
+            mob = produce.get_mob_date(
+                region,
+                aggType,
+                get = False,
+                refresh = True,
+                override = False
+                )
+            produce.make_mob_plots(
+                mob,
+                region,
+                aggType,
+                )
+            produce.make_mob_dateMap(
+                mob,
+                region,
+                aggType,
+                )
+        except NoData:
+            print("No data currently available for:", region)
+        except:
+            print("Something went wrong with:", region, aggType)
 produce.make_meldash()
