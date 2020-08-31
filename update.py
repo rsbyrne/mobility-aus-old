98b4e459146bd2cec985450d72d35341109811b4
rsbyrne <rohan.byrne@gmail.com>
2020-08-31 05:02:50 +0000

    Automatic push.

--- update.py
+++ update.py
@@ -14,35 +14,35 @@ from load import NoData
 # aggTypes = {'lga',}
 
 regions = {'vic', 'mel', 'nsw', 'syd'}
-aggTypes = {'lga', 'sa2', 'postcodes'}
+aggTypes = {'lga', 'sa2'}
 
 # # absLookup = produce.get_abs_lookup(aggTypes, refresh = True)
 # absLookup = produce.get_abs_lookup({'lga', 'sa2'}, refresh = True)
 
-for region in regions:
-    for aggType in aggTypes:
-        try:
-            mob = produce.get_mob_date(
-                region,
-                aggType,
-                get = False,
-                refresh = True,
-                override = False
-                )
-            produce.make_mob_plots(
-                mob,
-                region,
-                aggType,
-                )
-            produce.make_mob_dateMap(
-                mob,
-                region,
-                aggType,
-                )
-        except NoData:
-            print("No data currently available for:", region)
-        except:
-            print("Something went wrong with:", region, aggType)
+# for region in regions:
+#     for aggType in aggTypes:
+#         try:
+#             mob = produce.get_mob_date(
+#                 region,
+#                 aggType,
+#                 get = False,
+#                 refresh = True,
+#                 override = False
+#                 )
+#             produce.make_mob_plots(
+#                 mob,
+#                 region,
+#                 aggType,
+#                 )
+#             produce.make_mob_dateMap(
+#                 mob,
+#                 region,
+#                 aggType,
+#                 )
+#         except NoData:
+#             print("No data currently available for:", region)
+#         except:
+#             print("Something went wrong with:", region, aggType)
 
 produce.make_meldash()
 
