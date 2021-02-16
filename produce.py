003c1bff5f96777a1b3b398e2b1c18a39c4eec04
root <root@miranda-1.novalocal>
2021-02-16 06:37:58 +0000

    Automatic push.

--- produce.py
+++ produce.py
@@ -48,7 +48,7 @@ MELVIC_ANNOTATIONS = [
     ('2020-12-26', 'Boxing\nDay', (0, 30)),
     ('2021-01-01', "New Year's\nDay", (0, -30)),
     ('2021-01-26', "National\nholiday", (0, -30)),
-    ('2021-02-12', "Circuit\nbreaker", (0, 30)),
+    ('2021-02-13', "Circuit\nbreaker", (0, -30)),
     ]
 
 def get_abs_lookup(sources, refresh = False):
